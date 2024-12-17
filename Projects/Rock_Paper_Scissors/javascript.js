4b883e644c1cab247cbe73d236812ab6964c0948
selmansezer <selmansezer.cs@gmail.com>
2024-12-17 13:54:16 +0300

    Do the assignment

--- Projects/Rock_Paper_Scissors/javascript.js
+++ Projects/Rock_Paper_Scissors/javascript.js
@@ -39,53 +39,79 @@ function playGame()
         if (humanChoice === 'rock' && computerChoice === 'paper') {
             computerScore += 1;
             console.log(`“You lose! ${capitalizeInitial(computerChoice)} beats ${capitalizeInitial(humanChoice)}.`);
-            alert("computer won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
-            
         }
         else if (humanChoice === 'paper' && computerChoice === 'scissors') {
             computerScore += 1;
             console.log(`“You lose! ${capitalizeInitial(computerChoice)} beats ${capitalizeInitial(humanChoice)}.`);
-            alert("computer won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
         }
         else if (humanChoice === 'scissors' && computerChoice === 'rock') {
             computerScore += 1;
             console.log(`“You lose! ${capitalizeInitial(computerChoice)} beats ${capitalizeInitial(humanChoice)}.`);
-            alert("computer won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
         }
         else if (humanChoice === 'scissors' && computerChoice === 'paper') {
             humanScore += 1;
             console.log(`“You win! ${capitalizeInitial(humanChoice)} beats ${capitalizeInitial(computerChoice)}.`);
-            alert("You won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
         }
     
         else if (humanChoice === 'rock' && computerChoice === 'scissors') {
             humanScore += 1;
             console.log(`“You win! ${capitalizeInitial(humanChoice)} beats ${capitalizeInitial(computerChoice)}.`);
-            alert("You won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
         }
         else if (humanChoice === 'paper' && computerChoice === 'rock') {
             humanScore += 1;
             console.log(`“You win! ${capitalizeInitial(humanChoice)} beats ${capitalizeInitial(computerChoice)}.`);
-            alert("You won the round")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
-        }
-        else
-        {
-            alert("Both played the same hand ")
-            alert(`Current score is you : ${humanScore}, computer ${computerScore}`)
-        }
-  
-    
+        }    
     }
 
+    const buttonRock = document.createElement('button');
+    const buttonPaper = document.createElement('button');
+    const buttonScissors = document.createElement('button');
 
-    humanScore > computerScore ?  console.log(`You won the game !`) : console.log("Computer won the game !!");
+    buttonRock.textContent = 'rock';
+    buttonPaper.textContent = 'paper';
+    buttonScissors.textContent = 'scissors';
+
+
+    buttonRock.addEventListener('click', ()=>{
+        const compChoice = getComputerChoice();
+        playRound('rock', compChoice);
+    });
+
+    buttonPaper.addEventListener('click', ()=>{
+        const compChoice = getComputerChoice();
+        playRound('paper', compChoice);
+    });
+
+    buttonScissors.addEventListener('click', ()=>{
+        const compChoice = getComputerChoice();
+        playRound('scissors', compChoice);
+    });
+
+    // creating div and adding the buttons to it
+    const div = document.createElement('div');
+    div.appendChild(buttonRock);
+    div.appendChild(buttonPaper);
+    div.appendChild(buttonScissors);
+
+    // create a method to display the score
+    div.addEventListener('click', (e)=>{
+        let p = document.createElement('p');
+        p.textContent = `The score is you:${humanScore}, computer:${computerScore}`;
+        div.appendChild(p);
+        if (humanScore ==5) {
+            alert('You won');
+            humanScore = 0;
+            computerScore = 0;
+        }
+        if (computerScore == 5) {
+            alert('computer won');
+            humanScore = 0;
+            computerScore = 0;
+        }
+    })
+    const body = document.querySelector('body');
+    body.appendChild(div);
+
 }
 
 playGame();
-
