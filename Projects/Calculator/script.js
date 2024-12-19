1fb70442ba452dead08482553c787bd121639555
selmansezer <selmansezer.cs@gmail.com>
2024-12-19 08:59:51 +0300

    Calculator Add a textbox to use as the screen Implement the 'operate' function Make some small changes in CSS

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,3 +1,12 @@
+// GLOBAL VARIBLES
+let value = 0;
+let screen = value.toString();
+let operator = '';
+let leftOperand = 0;
+let rightOperand = 0;
+
+
+
 // OPERATIONS
 const add = function(a,b) {
 	return a+b
@@ -7,7 +16,6 @@ const subtract = function(a,b) {
 	return a-b
 };
 
-
 const multiply = function(args) {
   return args.reduce((a,b)=> { return a*b}, 1);
 };
@@ -16,3 +24,54 @@ const div = function(a,b)
 {
     return a/b;
 }
+
+function operate()
+{
+  if (operator == '+') {
+    value = add(leftOperand, rightOperand);
+  }
+  else if (operator == '-') {
+    value = subtract(leftOperand, rightOperand);
+  }
+  else if (operator == '*') {
+    value = multiply(leftOperand, rightOperand);
+  }
+  else if (operator == '/') {
+    value = div(leftOperand, rightOperand);
+  }
+  else if (operator == '%') {
+    value = leftOperand % rightOperand;
+  }
+}
+
+
+// Selectin buttons
+const btnAc = document.querySelector('#ac');
+const btnSeven = document.querySelector('#seven');
+const btnFour = document.querySelector('#four');
+const btnOne = document.querySelector('#one');
+const btnDelete = document.querySelector('#delete');
+
+const btnSign = document.querySelector('#sign');
+const btnEight = document.querySelector('#eight');
+const btnFive = document.querySelector('#five');
+const btnTwo = document.querySelector('#two');
+const btnZero = document.querySelector('#zero');
+
+const btnModular  = document.querySelector('#modular');
+const btnNine     = document.querySelector('#none');
+const btnSix      = document.querySelector('#six');
+const btnThree    = document.querySelector('#three');
+const btnPoint    = document.querySelector('#point');
+
+const btnDiv       = document.querySelector('#div');
+const btnMult      = document.querySelector('#mult');
+const btnSub       = document.querySelector('#sub');
+const btnAdd       = document.querySelector('#add');
+const btnCalc      = document.querySelector('#calc');
+
+
+
+
+
+
